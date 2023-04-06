457d01885e67b0a172d6292b4185b57d25d8051e
As <volodko2000@bk.ru>
2023-04-06 17:50:12 +0300

    menuPage 50% complite

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,37 +3,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //= components/
 
+    let mainPage = document.querySelector('.main')
 
-    let mainMenuContainer = document.querySelector('.main-menu__containerHelper')
-    let mainMenuBtns = document.querySelectorAll('.main-menu__menuBtn')
+    if (mainPage) {
+        let mainMenuContainer = document.querySelector('.main-menu__containerHelper')
+        let mainMenuBtns = document.querySelectorAll('.main-menu__menuBtn')
 
-    mainMenuBtns.forEach((e, id) => {
-        e.addEventListener('click', el => {
-            mainMenuBtns.forEach(e => {
-                e.classList.remove('activeMenuBtn')
-                e.classList.remove('activeCutCornerLast')
-                e.classList.remove('activeCutCornerFirst')
+        mainMenuBtns.forEach((e, id) => {
+            e.addEventListener('click', el => {
+                mainMenuBtns.forEach(e => {
+                    e.classList.remove('activeMenuBtn')
+                    e.classList.remove('activeCutCornerLast')
+                    e.classList.remove('activeCutCornerFirst')
+                })
+                e.classList.add('activeMenuBtn')
+                if (id === mainMenuBtns.length - 1 && window.matchMedia("(min-width: 500px)").matches) {
+                    mainMenuBtns[id - 1].classList.add('activeCutCornerLast')
+                } else if (id === 0 && window.matchMedia("(min-width: 500px)").matches) {
+                    mainMenuBtns[1].classList.add('activeCutCornerFirst')
+                } else {
+
+                    if (window.matchMedia("(min-width: 500px)").matches) {
+                        mainMenuBtns[id - 1].classList.add('activeCutCornerLast')
+                        mainMenuBtns[id + 1].classList.add('activeCutCornerFirst')
+                    }
+
+                }
+                mainMenuContainer.innerHTML = e.nextElementSibling.innerHTML
             })
-            e.classList.add('activeMenuBtn')
-            if (id === mainMenuBtns.length - 1) {
-                mainMenuBtns[id - 1].classList.add('activeCutCornerLast')
-            }else if (id === 0) {
-                mainMenuBtns[1].classList.add('activeCutCornerFirst')
-            }else {
-                mainMenuBtns[id - 1].classList.add('activeCutCornerLast')
-                mainMenuBtns[id + 1].classList.add('activeCutCornerFirst')
-            }
-            mainMenuContainer.innerHTML = e.nextElementSibling.innerHTML
         })
-    })
 
-    mainMenuBtns[0].click()
+        mainMenuBtns[0].click()
+    }
 
     let dropdownBtn = document.querySelector('.header-languageContainer svg')
     let dropdownContent = document.querySelector('.header-languageContainer ul')
 
     dropdownBtn.addEventListener('click', e => {
-        
+
         dropdownContent.classList.toggle('increaceHeight')
     })
 
@@ -50,6 +57,43 @@ document.addEventListener("DOMContentLoaded", () => {
     crossBtn.addEventListener('click', e => {
         headerModal.classList.remove('header-modal__show')
         document.body.classList.remove('stopScroll')
+    })
+
+
+    let menu = document.querySelectorAll('.menu-content__btnContainer button')
+    let menuContent = document.querySelector('.menu-content__helperContainer')
+    let menuLeftContainer = document.querySelector('.menu-content__leftContainer')
+    let menuCenterContainer = document.querySelector('.menu-content__centerContainer')
+    let menuRightContainer = document.querySelector('.menu-content__rightContainer')
+
+    menu.forEach((e) => {
+        e.addEventListener('click', el => {
+            // e.nextElementSibling.querySelectorAll('ul')
+            const array = ['элемент 1', 'элемент 2', 'элемент 3', 'элемент 4', 'элемент 5'];
+            const container1 = document.getElementById('container1');
+            const container2 = document.getElementById('container2');
+            const container3 = document.getElementById('container3');
+
+            let i = 0;
+            let containerIndex = 1;
+
+            while (i < array.length) {
+                const element = document.createElement('div');
+                element.textContent = array[i];
+
+                if (containerIndex === 1) {
+                    menuLeftContainer.appendChild(element);
+                } else if (containerIndex === 2) {
+                    menuCenterContainer.appendChild(element);
+                } else {
+                    menuRightContainer.appendChild(element);
+                }
+
+                containerIndex = (containerIndex % 3) + 1;
+
+                i++;
+            }
+        })
     })
 
 })
