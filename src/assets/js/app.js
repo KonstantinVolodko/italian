3af4ed04bf60345fea93bb30f2600e9dcd31a796
As <volodko2000@bk.ru>
2023-04-07 17:52:15 +0300

    end

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -65,40 +65,94 @@ document.addEventListener("DOMContentLoaded", () => {
     let menuLeftContainer = document.querySelector('.menu-content__leftContainer')
     let menuCenterContainer = document.querySelector('.menu-content__centerContainer')
     let menuRightContainer = document.querySelector('.menu-content__rightContainer')
+    let menuPage = document.querySelector('.menu')
 
-    menu.forEach((e) => {
+
+    if (menuPage) {
+        
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+            menu.forEach((e) => {
+                e.addEventListener('click', el => {
+                    let ulArray = e.nextElementSibling.querySelectorAll('ul')
+                    let i = 0;
+                    let containerIndex = 1;
+        
+                    menuLeftContainer.innerHTML = ""
+                    menuCenterContainer.innerHTML = ""
+                    menuRightContainer.innerHTML = ""
+                    
+        
+                    menu.forEach(el => {
+                        el.classList.remove('activeTab')
+                    })
+                    e.classList.add('activeTab')
+                    
+        
+                    while (i < ulArray.length) {
+                        const element = document.createElement('ul');
+                        element.innerHTML = ulArray[i].innerHTML;
+        
+                        
+        
+                        if (containerIndex === 1) {
+                            menuLeftContainer.appendChild(element);
+                        } else if (containerIndex === 2) {
+                            menuCenterContainer.appendChild(element);
+                        }else {
+                            menuRightContainer.appendChild(element);
+                        }
+        
+                        containerIndex = (containerIndex % 3) + 1;
+        
+                        i++;
+                    }
+                })
+            })
+        }else if (window.matchMedia("(max-width: 1024px)").matches) {
+            menu.forEach(e => {
+                e.addEventListener('click', el => {
+                    menu.forEach(el => {
+                        el.classList.remove('activeTab')
+                    })
+                    e.classList.add('activeTab')
+                    menuContent.innerHTML = e.nextElementSibling.innerHTML
+                })
+            })
+        }
+
+
+        menu[0].click()
+    }
+
+
+
+    let modal = document.querySelector('.modal')
+    let openModal = document.querySelectorAll('.openModal')
+    let modalCross = document.querySelector('.modal-crossContainer')
+    let formContainer = document.querySelector('.modal-formContainer')
+    let thanksContainer = document.querySelector('.modal-thanksContainer')
+    let sendBtn = document.querySelectorAll('.modal-sendBtn')
+
+    
+    openModal.forEach(e => {
         e.addEventListener('click', el => {
-            let ulArray = e.nextElementSibling.querySelectorAll('ul')
-            let i = 0;
-            let containerIndex = 1;
-            
-
-            while (i < ulArray.length) {
-                const element = document.createElement('ul');
-                element.innerHTML = ulArray[i].innerHTML;
-
-                
-
-                if (containerIndex === 1) {
-                    menuLeftContainer.appendChild(element);
-                } else if (containerIndex === 2) {
-                    menuCenterContainer.appendChild(element);
-                }else {
-                    menuRightContainer.appendChild(element);
-                }
-
-                containerIndex = (containerIndex % 3) + 1;
-
-                i++;
-            }
-
-            if (e.nextElementSibling.innerHTML === '') {
-                console.log('hellp')
-            }
+            modal.classList.add('showModal')
         })
     })
 
-    menu[0].click()
+    modalCross.addEventListener('click', el => {
+        modal.classList.remove('showModal')
+        formContainer.classList.remove('hidden')
+        thanksContainer.classList.remove('show')
+    })
+
+    sendBtn.forEach(e => {
+        e.addEventListener('click', el => {
+            formContainer.classList.add('hidden')
+            thanksContainer.classList.add('show')
+        })
+    })
+    
 
 })
 
